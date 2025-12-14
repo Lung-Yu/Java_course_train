58610ddfdcbaf9b2960cc1c3c16afb4179d40bb7
tygrus <workfile975@gmail.com>
2025-12-14 14:56:26 +0800

    重構 HelloWorld 類別，新增使用者登入驗證功能

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 
 public class HelloWorld{
+
+    private static final String[][] ACCOUNTS = {
+        {"user", "user"},
+        {"peter", "123"},
+        {"admin", "admin"}
+    };
+
+    private static boolean validateLogin(String account, String password) {
+        for (String[] credentials : ACCOUNTS) {
+            if (credentials[0].equals(account) && credentials[1].equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         String account = scanner.next();
         String password = scanner.next();
-
-        if(account.equals("user") && password.equals("user")){
+        
+        if(validateLogin(account, password)){
             System.out.println("login success.");
         }else{
-            System.out.println("login fail");
+            System.err.println("login fail.");
         }
         
        scanner.close();
