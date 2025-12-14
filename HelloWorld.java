8cf902914644cabd9fd613881f41fbbad1b927db
tygrus <workfile975@gmail.com>
2025-12-14 14:53:19 +0800

    新增 Users 類別，實現使用者名稱和密碼的設置與獲取功能

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,11 @@
 public class HelloWorld{
     public static void main(String[] args) {
 
+        Users users = new Users();
+
+        users.setPassword("user");
+        users.setUsername("user");
+
        System.out.println("hello world");
        
     }
