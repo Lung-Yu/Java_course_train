ee823ba61ea6c3f94afa9b571dd95b54956d1552
tygrus <workfile975@gmail.com>
2025-12-14 14:22:09 +0800

    移除 HelloWorld 類別中的使用者輸入功能，僅保留基本的 Hello World 輸出

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,9 @@
-import java.util.Scanner;
 
 public class HelloWorld{
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Please key in your name : ");
-        
-        String name = scanner.next();
-
-        System.out.println("Hello " + name);
-
-        scanner.close();
+       System.out.println("hello world");
+       
     }
 }
 
